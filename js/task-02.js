0db3489db00891941d3d5140255adb0dbfb09f5c
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-06-12 13:03:21 +0300

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientItemListEl = [];
+
+ingredients.forEach((ingredient) => {
+  const addInredientEl = document.createElement('li');
+  addInredientEl.textContent = `${ingredient}`;
+  addInredientEl.classList.add('item');
+  ingredientItemListEl.push(addInredientEl);
+});
+
+const allIngridientsEl = document.querySelector('#ingredients');
+allIngridientsEl.append(...ingredientItemListEl);
+console.log(allIngridientsEl);
