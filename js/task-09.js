eaaa97cbf1e11c6370dc6980849f5816699e93d1
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-06-16 13:57:03 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  bodyEl: document.querySelector('body'),
+  buttonEl: document.querySelector('.change-color'),
+  textCollorEl: document.querySelector('.color'),
+};
+
+refs.buttonEl.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  refs.bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  refs.textCollorEl.textContent = `${getRandomHexColor()}`;
+}
